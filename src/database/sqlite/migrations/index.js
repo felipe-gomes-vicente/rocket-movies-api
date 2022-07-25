076ca73cd82d7b9c962063f68e4b28e042565062
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-07-25 15:46:10 -0300

    project refactoring

--- src/database/sqlite/migrations/index.js
+++ src/database/sqlite/migrations/index.js
@@ -2,14 +2,11 @@ const sqliteConnection = require("../../sqlite");
 const createUsers = require("./createUsers");
 
 async function migrationsRun() {
-  const schemas = [
-    createUsers
-  ].join("");
+  const schemas = [createUsers].join("");
 
   sqliteConnection()
-    .then(db => db.exec(schemas))
-    .catch(error => console.error(error));
-
+    .then((db) => db.exec(schemas))
+    .catch((error) => console.error(error));
 }
 
 module.exports = migrationsRun;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 
-const  usersRouter  = require("./users.routes");
-const  notesRouter  = require("./notes.routes");
-const  tagsRouter  = require("./tags.routes");
+const usersRouter = require("./users.routes");
+const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/movie_notes", notesRouter);
 routes.use("/tags", tagsRouter);
 
